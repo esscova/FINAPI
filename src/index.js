601907534c90848cd649f61f52332029049ca227
wellington moreira <wsantos08@hotmail.com>
2023-10-23 20:44:22 -0300

    adicao de middleware, req post para depositos e package com adicao de module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-const express = require('express');
-const {v4: uuidv4} = require("uuid")
+import express, { json, request, response } from 'express';
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
-app.use(express.json());
-app.listen(3333);
+app.use(json());
+
 
 const customers = [];
+
+//middleware
+function verifyIfExistsAccountCPF (request,response,next){
+    const {cpf} = request.headers;
+    const customer = customers.find((customer) => customer.cpf === cpf);
+    if(!customer){
+        return response.status(400).json({error:"Customer not found"});
+    }
+    request.customer = customer;
+    return next();
+}
 
 app.post('/account', (request,response)=>{
     const {cpf, name} = request.body;
@@ -27,4 +38,28 @@ app.post('/account', (request,response)=>{
         statement:[]
     });
     return response.status(201).send();
-})
+});
+
+//app.use(verifyIfExistsAccountCPF);
+
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {  
+    const {customer} = request;  
+    return response.json(customer.statement);
+});
+
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+    const {description, amount} = request.body;
+    const {customer} = request;
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+    customer.statement.push(statementOperation);
+    return response.status(201).send();
+});
+
+app.listen(3333, () => {
+    console.log('🛸 servidor rodando de http://localhost:3333');
+});
