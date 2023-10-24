daec8323acca6c8641b2bf940d2ce31c086b34bc
wellington moreira <wsantos08@hotmail.com>
2023-10-24 19:00:32 -0300

    add req delete account e get balance

--- src/index.js
+++ src/index.js
@@ -111,6 +111,18 @@ app.get('/account', verifyIfExistsAccountCPF, (request,response) => {
     return response.json(customer);
 });
 
+app.delete('/account',verifyIfExistsAccountCPF,(request,response) => {
+    const {customer} = request;
+    customers.splice(customer,1);
+    return response.status(200).json(customers);
+});
+
+app.get('/balance',verifyIfExistsAccountCPF,(request,response) => {
+    const {customer} = request;
+    const balance = getBalance(customer.statement);
+    return response.json(balance);
+});
+
 app.listen(3333, () => {
     console.log('🛸 servidor rodando de http://localhost:3333');
 });
