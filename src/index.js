25c269d75dac5d400e92c1a6b22df77793e256cd
wellington moreira <wsantos08@hotmail.com>
2023-10-24 15:20:30 -0300

    req saques, funcao conferir saldos

--- src/index.js
+++ src/index.js
@@ -17,6 +17,16 @@ function verifyIfExistsAccountCPF (request,response,next){
     request.customer = customer;
     return next();
 }
+function getBalance(statement){
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type==='credit'){
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+}, 0);
+    return balance;
+}
 
 app.post('/account', (request,response)=>{
     const {cpf, name} = request.body;
@@ -59,6 +69,25 @@ app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
     customer.statement.push(statementOperation);
     return response.status(201).send();
 });
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+    const {amount} = request.body;
+    const {customer} = request;
+    const balance = getBalance(customer.statement);
+
+    if(balance<amount){
+        return response.status(400).json({error:"Insufficient funds"});
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    };
+    
+    customer.statement.push(statementOperation);
+    return response.status(201).send();
+})
 
 app.listen(3333, () => {
     console.log('🛸 servidor rodando de http://localhost:3333');
